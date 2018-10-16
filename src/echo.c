b45eecda6103af61857667da3c3ac287d7a056c3
Valentin Debon <valentin@debon.net>
2018-10-17 01:04:59 +0200

    Semi-compliant echo, missing a good code and escape sequences

--- src/echo.c
+++ src/echo.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <alloca.h>
+#ifdef ECHO_XSI
+#include <stdbool.h>
+#else
+#include <string.h>
+#endif
 
 #include <heylel/core.h>
 
 static char *buffer;
 static char *bufferpos;
 static char *bufferend;
+static char *echoname;
 
 static void
 echo_flush(void) {
-	write(STDOUT_FILENO, buffer, bufferpos - buffer);
-	bufferpos = buffer;
+	ssize_t writeval = write(STDOUT_FILENO, buffer, bufferpos - buffer);
+	if(writeval <= 0) {
+		if(writeval == -1) {
+			perror(echoname);
+		}
+		exit(1);
+	}
+	bufferpos -= writeval;
 }
 
 /**
@@ -21,36 +34,60 @@ echo_flush(void) {
  */
 int
 main(int argc,
-	char ** const argv) {
+	char **argv) {
 	size_t outsize = io_blocksize(STDOUT_FILENO);
 	buffer = alloca(outsize);
 	bufferpos = buffer;
 	bufferend = buffer + outsize;
+	echoname = *argv;
 	char **argpos = argv + 1;
 	char **argend = argv + argc;
+#ifdef ECHO_XSI
+#define trailing '\n'
+#else
+	char trailing = '\n';
+	if(argc > 1
+		&& strcmp(*argpos, "-n") == 0) {
+		/* Like a majority of unix-like systems,
+		 * we won't write trailing newline on -n */
+		trailing = '\0';
+		argpos += 1;
+	}
+#endif
 
 	while(argpos != argend) {
 		if(bufferpos == bufferend) {
 			echo_flush();
 		}
 
-		if(**argpos == '\0') {
+		switch(**argpos) {
+		case '\0':
 			*bufferpos = ' ';
 			argpos += 1;
-		} else {
+			break;
+		default:
 			*bufferpos = **argpos;
 			*argpos += 1;
+			break;
 		}
 
 		bufferpos += 1;
 	}
 
-	if(bufferpos != buffer) {
-		bufferpos[-1] = '\n';
-	} else {
-		*bufferpos = '\n';
-		bufferpos += 1;
+#ifndef ECHO_XSI
+	if(trailing != '\0') {
+#endif
+		if(bufferpos != buffer) {
+			bufferpos[-1] = trailing;
+		} else {
+			*bufferpos = trailing;
+			bufferpos += 1;
+		}
+#ifndef ECHO_XSI
+	} else if(bufferpos != buffer) {
+		bufferpos -= 1;
 	}
+#endif
 
 	echo_flush();
 
